6b6af0e1c74913eb378e40b1b293cbbfb1fcb600
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2025-02-23 16:29:15 -0500

    search Box styling and add image functionality

--- Material-UI/src/SearchBox.jsx
+++ Material-UI/src/SearchBox.jsx
@@ -2,47 +2,73 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 
-export default function SearchBox() {
+import "./SearchBox.css";
+
+export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
   let API_URL = "http://api.openweathermap.org/geo/1.0/direct";
   let API_KEY = "50127ef28a9d36f0ab26b7d1781e1a11";
   let API_URL2 = "https://api.openweathermap.org/data/2.5/weather";
 
   async function ApiCall(city) {
-    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`);
+    try {
+      let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`);
+      let jsonResponse = await response.json();
+      let { lat, lon } = jsonResponse[0];
+      let res = await fetch(
+        `${API_URL2}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      );
+      let json = await res.json();
+
+      let imageURL = await GetImageforCity(city);
+
+      let weather = {
+        city: city,
+        temp: json.main.temp,
+        temp_min: json.main.temp_min,
+        temp_max: json.main.temp_max,
+        humidity: json.main.humidity,
+        feels_like: json.main.feels_like,
+        image: imageURL,
+      };
+
+      return weather;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async function GetImageforCity(city) {
+    let accessToken = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+    let url = import.meta.env.VITE_APP_UNSPLASH_URL;
+    let apiUrl = `${url}?client_id=${accessToken}&query=${city}`;
+
+    let response = await fetch(apiUrl);
     let jsonResponse = await response.json();
-    //console.log(jsonResponse);
-    let { lat, lon } = jsonResponse[0];
 
-    let res = await fetch(
-      `${API_URL2}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    );
-    let json = await res.json();
-
-    let weather = {
-      temp: json.main.temp,
-      temp_min: json.main.temp_min,
-      temp_max: json.main.temp_max,
-      humidity: json.main.humidity,
-      feels_like: json.main.feels_like,
-    };
-
-    console.log(weather);
+    let imageURL = jsonResponse.results[0].urls.small;
+    return imageURL;
   }
 
   function handleChange(event) {
     setCity(event.target.value);
   }
-  function handleSubmit(event) {
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    ApiCall(city);
-    setCity("");
+    try {
+      let newInfo = await ApiCall(city);
+      setCity("");
+      updateInfo(newInfo);
+    } catch (err) {
+      setError(true);
+    }
   }
 
   return (
     <>
       <div className="SearchBoxDiv">
-        <h3>Weather</h3>
         <form onSubmit={handleSubmit}>
           <TextField
             id="City"
@@ -54,9 +80,16 @@ export default function SearchBox() {
           />
           <br></br>
           <br></br>
-          <Button variant="contained" type="submit">
-            Search
-          </Button>
+          <div className="SearchBoxButton">
+            <Button variant="contained" type="submit">
+              Search
+            </Button>
+            {error && (
+              <b>
+                <p style={{ color: "red" }}>No such Place exist in our API</p>
+              </b>
+            )}
+          </div>
         </form>
       </div>
     </>
