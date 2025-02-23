7a3eabc59e049842b09e4fd45bbae90f843a9f2a
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2025-02-23 16:29:37 -0500

    weather app

---
+++ Material-UI/src/WeatherApp.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import InfoBox from "./InfoBox";
+import SearchBox from "./SearchBox";
+
+export default function WeatherApp() {
+  const [weatherInfo, setWeatherInfo] = useState({
+    city: "Delhi",
+    feels_like: 26.03,
+    humidity: 61,
+    temp: 26.03,
+    temp_max: 26.03,
+    temp_min: 26.03,
+  });
+
+  let updateInfo = (result) => {
+    setWeatherInfo(result);
+  };
+  return (
+    <>
+      <h1>{import.meta.env.VITE_UNSPLASH_APPLICATION_ID}</h1>
+      <div style={{ textAlign: "center" }}>
+        <h2> Weather App</h2>
+        <SearchBox updateInfo={updateInfo}></SearchBox>
+        <InfoBox info={weatherInfo}></InfoBox>
+      </div>
+    </>
+  );
+}
